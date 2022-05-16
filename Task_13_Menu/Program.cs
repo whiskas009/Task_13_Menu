77ca1eedfaf6ce04f8db13d89c347d514ed9b359
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-16 21:30:30 +0300

    #1

---
+++ Task_13_Menu/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_Menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberForEscape = 0;
+            string firstName = "...";
+            string secondName = "...";
+            string userName = "...";
+            string age = "...";
+            string setPassword = "";
+            string enterPassword = "";
+
+            while (numberForEscape != 1)
+            {
+                
+                if (enterPassword == setPassword)
+                {
+                    while (enterPassword == setPassword)
+                    {
+                        Console.WriteLine($"\nВаш имя: {firstName} \nВаша фамилия: {secondName} \nВаш псевдоним: {userName} \nВаш возраст: {age}\n");
+                        Console.WriteLine("Выберите действие: \n1. Установить имя \n2. Установить фамилию \n3. Установить псевдоним \n4. Указать возраст \n5. Стереть все данные \n6. Установить пароль\n7. Выход\n");
+                        
+                        int menuNumber = int.Parse(Console.ReadLine());
+
+                        switch (menuNumber)
+                        {
+                            case 1:
+                                Console.WriteLine("\nВведите ваше имя:");
+                                firstName = Console.ReadLine();
+                                break;
+
+                            case 2:
+                                Console.WriteLine("\nВведите вашу фамилию:");
+                                secondName = Console.ReadLine();
+                                break;
+
+                            case 3:
+                                Console.WriteLine("\nВведите ваш псевоним:");
+                                userName = Console.ReadLine();
+                                break;
+
+                            case 4:
+                                Console.WriteLine("\nВведите ваш возраст:");
+                                age = Console.ReadLine();
+                                break;
+
+                            case 5:
+                                firstName = "...";
+                                secondName = "...";
+                                userName = "...";
+                                age = "...";
+                                break;
+
+                            case 6:
+                                Console.WriteLine("\nВведите ваш пароль:");
+                                setPassword = Console.ReadLine();
+                                Console.WriteLine("Готово!\n");
+                                break;
+                                
+                            case 7:
+                                numberForEscape = 1;
+                                setPassword = " ";
+                                break;
+                        }
+
+                    }
+                }
+
+                if (numberForEscape == 1) break;
+                Console.WriteLine("Введите пароль: или введите 0 для выхода");
+                enterPassword = Console.ReadLine();
+                
+                switch (enterPassword)
+                {
+                    case "0":
+                        numberForEscape = 1;
+                        break;
+                }  
+            }
+
+            
+            
+        }
+
+    }
+}
---
+++ Task_13_Menu/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task_13_Menu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_13_Menu")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8f8c2283-db02-421c-87cd-51e3a191faf9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
